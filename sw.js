a90566420eaef484704a97e7ff1141f01872bf09
IT-Rexir <lazyjul64@gmail.com>
2026-07-17 21:48:36 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "attendance-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      "./",
+      "./index.html"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
